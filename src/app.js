8ff4a461de96593f352a084f334e18eae8f85ad9
mykola-davydenko <nikolay.davydenko@eleve.efc.edu.ge>
2025-10-26 11:32:29 +0200

    fix

---
+++ src/app.js
@@ -0,0 +1,14 @@
+// import {add, minus, multiply, divide} from "./js/calculator";
+// import {minus} from "./js/calculator";
+
+// console.log(add(10,5));
+// console.log(minus(15, 10));
+// console.log(multiply(2,3));
+// console.log(divide(10,5));
+
+// import {nano-id} from "nano-id";
+import *as calc from './js/calculator';
+console.log(calc.add(2,5));
+console.log(calc.minus(2,5));
+console.log(calc.multiply(2,5));
+console.log(calc.divide(2,5));
